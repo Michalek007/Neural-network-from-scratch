5cbd51b5900ead0d8106e73b44b6ee3d4859dfc9
Michalek007 <nmichal02@op.pl>
2024-01-09 22:26:43 +0100

    Added: ActivationReLU class, spiral_data, activation_relu_example, layer_dense_example functions

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
+import numpy as np
+
 from layer_dense import LayerDense
+from activation_relu import ActivationReLU
 
 
-if __name__ == '__main__':
+def spiral_data(points: int, classes: int):
+    X = np.zeros((points*classes, 2))
+    y = np.zeros(points*classes, dtype='uint8')
+    for class_number in range(classes):
+        ix = range(points*class_number, points*(class_number+1))
+        r = np.linspace(0.0, 1, points)  # radius
+        t = np.linspace(class_number*4, (class_number+1)*4, points) + np.random.randn(points)*0.2
+        X[ix] = np.c_[r*np.sin(t*2.5), r*np.cos(t*2.5)]
+        y[ix] = class_number
+    return X, y
+
+
+def layer_dense_example():
     X = [
         [1, 2, 3, 2.5],
         [2.0, 5.0, -1.0, 2.0],
@@ -15,3 +30,19 @@ if __name__ == '__main__':
     layer2.forward(layer1.output)
 
     print(layer2.output)
+
+
+def activation_relu_example():
+    X, y = spiral_data(points=100, classes=3)
+
+    layer1 = LayerDense(2, 5)
+    activation = ActivationReLU()
+
+    layer1.forward(X)
+    activation.forward(layer1.output)
+    print(activation.output)
+
+
+if __name__ == '__main__':
+    layer_dense_example()
+    activation_relu_example()
