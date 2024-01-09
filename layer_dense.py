e5dbd9c2481bd5f4009cbb9a0c20518b4d4a2a49
Michalek007 <nmichal02@op.pl>
2024-01-09 19:16:52 +0100

    Initial commit

---
+++ layer_dense.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+
+class LayerDense:
+    def __init__(self):
+        pass
+
+    def forward(self):
+        pass
---
+++ main.py
@@ -0,0 +1,5 @@
+from layer_dense import LayerDense
+
+
+if __name__ == '__main__':
+    pass
