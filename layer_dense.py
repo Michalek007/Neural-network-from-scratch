8ae705c4c67fd19a2594c2f79744014e62f1fadf
Michalek007 <nmichal02@op.pl>
2024-01-09 19:25:14 +0100

    Implemented LayerDense class, added sample usage in main.py

--- layer_dense.py
+++ layer_dense.py
@@ -1,9 +1,13 @@
 import numpy as np
 
+np.random.seed(0)
+
 
 class LayerDense:
-    def __init__(self):
-        pass
+    def __init__(self, n_inputs: int, n_neurons: int):
+        self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
+        self.output = None
 
-    def forward(self):
-        pass
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
--- main.py
+++ main.py
@@ -2,4 +2,16 @@ from layer_dense import LayerDense
 
 
 if __name__ == '__main__':
-    pass
+    X = [
+        [1, 2, 3, 2.5],
+        [2.0, 5.0, -1.0, 2.0],
+        [-1.5, 2.7, 3.3, -0.8]
+    ]
+
+    layer1 = LayerDense(4, 5)
+    layer2 = LayerDense(5, 2)
+
+    layer1.forward(X)
+    layer2.forward(layer1.output)
+
+    print(layer2.output)
