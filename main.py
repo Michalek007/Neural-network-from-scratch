8b57ac3a90d3eba898ebe0e4cda52f666722c15d
Michalek007 <nmichal02@op.pl>
2024-01-26 02:49:18 +0100

    Added: loss.py, neural_network_example function in main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import numpy as np
 from layer_dense import LayerDense
 from activation_relu import ActivationReLU
 from activation_softmax import ActivationSoftmax
+from loss import LossCategoricalCrossEntropy
 
 
 def spiral_data(points: int, classes: int):
@@ -61,7 +62,29 @@ def activation_soft_max_example():
     print(activation2.output)
 
 
+def neural_network_example():
+    X, y = spiral_data(points=100, classes=3)
+
+    layer1 = LayerDense(2, 3)
+    activation1 = ActivationReLU()
+
+    layer2 = LayerDense(3, 3)
+    activation2 = ActivationSoftmax()
+
+    loss_function = LossCategoricalCrossEntropy()
+
+    layer1.forward(X)
+    activation1.forward(layer1.output)
+
+    layer2.forward(activation1.output)
+    activation2.forward(layer2.output)
+
+    loss = loss_function.calculate(activation2.output, y)
+    print(loss)
+
+
 if __name__ == '__main__':
-    layer_dense_example()
-    activation_relu_example()
-    activation_soft_max_example()
+    # layer_dense_example()
+    # activation_relu_example()
+    # activation_soft_max_example()
+    neural_network_example()
